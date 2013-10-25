368c1e2261dd7026c53391f9b99f0633088583eb
Shinpei Maruyama <shinpeim@gmail.com>
2013-10-26 00:47:46 +0900

    Initial commit

---
+++ lib/rack/session/php.rb
@@ -0,0 +1,9 @@
+require "rack/session/php/version"
+
+module Rack
+  module Session
+    module Php
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rack/session/php/version.rb
@@ -0,0 +1,7 @@
+module Rack
+  module Session
+    module Php
+      VERSION = "0.0.1"
+    end
+  end
+end
